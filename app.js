52ede75386a94e42bae4c3f28d714b8de3e37576
Josh <jpeg.taylor@gmail.com>
2022-01-23 17:45:00 -0500

    moved pokemonData to function scope, removed unused or commented out code, added comments

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 'use strict';
-// fetch data from api
-// create object with data you want
-// loop over only the 1st 151 pokemon
 
+//reference search input element
 const searchBar = document.getElementById('searchBar');
-let pokemonData = [];
+//stores new array of objects containing pokemon image, id, name, and types
 let pokemonResults = [];
 
+//fetch pokemon data from Poke API
 const getPokemonData = async () => {
 
+    //stores raw data from Poke API
+    let pokemonData = [];
+
+    //loop through original 151 pokemon, fetch data and push to pokemonData array
     for (let i = 1; i <= 151; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         const res = await fetch(url);
@@ -17,8 +20,10 @@ const getPokemonData = async () => {
         pokemonData.push(data);
     }
 
+    //retrieve all results together 
     Promise.all(pokemonData).then( (results) => {
 
+        //retrieve only name, id, image, and type from each object
         const pokemon = results.map( (data) => ({
             name: data.name,
             id: data.id,
@@ -26,8 +31,10 @@ const getPokemonData = async () => {
             type: data.types.map((type) => type.type.name)
         }));
 
+        //push new array of pokemon to pokemonResults array usign spread operator
         pokemonResults.push(...pokemon);
         //console.log(pokemon);
+        //dispaly the pokemonlist in the DOM
         displayPokemon(pokemon);
         //displayPokemon(filteredPokemon);
 
@@ -36,17 +43,15 @@ const getPokemonData = async () => {
 }
 
 
-
+//function creates an HTML list item for each pokemon
 const displayPokemon = (pokemon) => {
 
     console.log(pokemon);
 
-    // const pokeType = pokemon.type;
-
-    // console.log(pokeType);
-
+    //reference unordered list element
     const pokeDexContainer = document.querySelector('.pokedex');
 
+    //create list item  with inner elements
     const generateHtml = (pokemon).map( (mon) => {
         return `
         <li class="poke-card">
@@ -80,42 +85,36 @@ const displayPokemon = (pokemon) => {
         `
     }).join('');
 
-    // console.log(generateHtml);
+    //insert list itmes inside of unordered list
     pokeDexContainer.innerHTML = generateHtml;
-
+     // console.log(generateHtml);
 }
 
 getPokemonData();
 
-
+//add event listener for search bar on key up
 searchBar.addEventListener('keyup', (event) => {
     console.log(event.target.value);
 
-    const searchEntry = event.target.value.toLowerCase();
+    //reference input string value from a user
+    const searchString = event.target.value.toLowerCase();
+    //reference input number value from a user
     const searchNumber = event.target.value;
-
+    
+    //filter through pokemonResults array and return pokemon by name id or type that match the users input
     const filteredPokemon = pokemonResults.filter( (mon) => {
 
-        // let pokemonType = mon.type.map( types => JSON.stringify(types));
-
-        // console.log(pokemonType);
         return (
-            mon.name.toLowerCase().includes(searchEntry) ||
+            mon.name.toLowerCase().includes(searchString) ||
             mon.id == searchNumber ||
-            JSON.stringify(mon.type).toLowerCase().includes(searchEntry)
+            JSON.stringify(mon.type).toLowerCase().includes(searchString)
         );
-
-        // if (mon.name.toLowerCase().includes(searchEntry)) {
-        //     return mon.name;
-        // }
-        // if (mon.id == searchNumber) {
-        //     return mon.id;
-        // }
         
     });
 
     console.log(filteredPokemon);
 
+    //display the list of filtered pokemon
     displayPokemon(filteredPokemon);
 
 });
