8120950bd035febb23acd661a21c864ce679f483
Josh <jpeg.taylor@gmail.com>
2022-02-24 01:07:09 -0500

    added displayDate function

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,8 +2,14 @@ import {getPokemonData} from './pokemon';
 
 import {clearBtn, clearSearch, searchPokeDex} from './search';
 
+import {displayDate} from './date';
+
 getPokemonData();
 
 searchPokeDex();
 
 clearBtn.addEventListener('click', clearSearch);
+
+// dateWrapper.addEventListener('DOMContentLoaded', displayDate);
+
+displayDate();
